aeb695de50bbced0cb20a0d0be401b2e846fcfda
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-10 15:08:10 +0200

    Exo3 end

--- App.js
+++ App.js
@@ -4,17 +4,65 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content1}>
+        <View style={styles.greenBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+
+      <View style={styles.content2}>
+        <View style={styles.purpleBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.pinkBox}></View>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  content1: {
+    flex: 1,
+    flexDirection: "row"
+  },
+
+  content2: {
+    flex: 1,
+    flexDirection: "column"
+  },
+
+  redBox: {
+    flex: 3,
+    backgroundColor: "red"
+  },
+
+  greenBox: {
+    flex: 2,
+    backgroundColor: "green"
+  },
+
+  blueBox: {
+    flex: 1,
+    backgroundColor: "blue"
+  },
+
+  purpleBox: {
+    flex: 3,
+    backgroundColor: "purple",
+  },
+  
+  yellowBox: {
+    flex: 2,
+    backgroundColor: "yellow",
+  },
+
+  pinkBox: {
+    flex: 1,
+    backgroundColor: "pink"
+  } 
 });
+
